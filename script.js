1db608bd98a47d2e84d632e1f356ea974dfdfc37
ahmadou18 <ah.gueye@laposte.net>
2017-09-05 14:34:42 +0200

    feat(effects):add some animations

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ function closeMenu() {
     document.querySelector('#side-menu').style.width = '0' + 'px';
     document.querySelector('.nav-open').style.display = 'block';
 }
+
+// animation on scroll
+function onScroll() {
+    let distance = window.pageYOffset;
+    let target = 300;
+
+    if (distance > target){
+        document.querySelector(".container").style.opacity = "1";
+    }else{
+        document.querySelector(".container").style.opacity = "0";
+    }
+}
+document.addEventListener('scroll', onScroll);
+
+function onScrollR() {
+    let distance = window.pageYOffset;
+    let target = 700;
+
+    if (distance > target){
+        document.querySelector(".banner-child").style.transform = "translate3d(0,0,0)";
+    }
+}
+document.addEventListener('scroll', onScrollR);
